ee5b9a223f6ac1f37aa0835515cd58d7a73ed6b8
Kaloyan Dimov <kaloyandimov99@gmail.com>
2022-10-02 19:51:49 +0300

    Add tests for new methods

--- test/bg/sofia/uni/fmi/mjt/crypto/storage/DefaultAssetStorageTest.java
+++ test/bg/sofia/uni/fmi/mjt/crypto/storage/DefaultAssetStorageTest.java
@@ -11,10 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -24,6 +27,7 @@ class DefaultAssetStorageTest {
     private static Asset bitcoin;
     private static Asset ethereum;
     private static Set<Asset> bitcoinSet;
+    private static Set<Asset> assetSet;
 
     private final AssetService serviceMock = Mockito.mock(AssetService.class);
     private final DefaultAssetStorage storage = new DefaultAssetStorage(serviceMock);
@@ -33,6 +37,7 @@ class DefaultAssetStorageTest {
         bitcoin = new Asset("BTC", "Bitcoin", true, 19424.470311714055519056008384);
         ethereum = new Asset("ETH", "Ethereum", true, 1039.3682961935640591450912866);
         bitcoinSet = Set.of(bitcoin);
+        assetSet = Set.of(bitcoin, ethereum);
     }
 
     @Test
@@ -63,6 +68,7 @@ class DefaultAssetStorageTest {
         when(serviceMock.getAssets()).thenReturn(bitcoinSet).thenReturn(Set.of(ethereum));
 
         storage.setUpdateInterval(0);
+        storage.setAssetLimit(150);
 
         assertEquals(bitcoin, storage.getAsset("BTC"), "Storage should contain bitcoin before update");
         assertEquals(ethereum, storage.getAsset("ETH"), "Storage should contain ethereum after update");
@@ -96,6 +102,51 @@ class DefaultAssetStorageTest {
         when(serviceMock.getAssets()).thenThrow(AssetServiceException.class);
 
         assertThrows(AssetStorageException.class, () -> storage.getAsset("BTC"), "AssetStorageException expected");
+
+        verify(serviceMock).getAssets();
+    }
+
+    @Test
+    void testGetAssets() throws AssetServiceException, AssetStorageException {
+        when(serviceMock.getAssets()).thenReturn(assetSet);
+
+        storage.setAssetLimit(150);
+
+        List<Asset> actual = storage.getAssets();
+
+        assertTrue(actual.containsAll(assetSet), "Actual list should contain all the expected elements");
+        assertTrue(assetSet.containsAll(actual), "Expected list should contain all the actual elements");
+
+        verify(serviceMock).getAssets();
+    }
+
+    @Test
+    void testGetAssetPricesSuccess() throws AssetServiceException, AssetStorageException {
+        when(serviceMock.getAssets()).thenReturn(assetSet);
+
+        storage.setAssetLimit(150);
+
+        Map<String, Double> assetPrices = storage.getAssetPrices(List.of("BTC", "ETH", "ANY"));
+
+        assertEquals(19424.47, assetPrices.get("BTC"), 0.01, "Bitcoin price should round to 19424.47");
+        assertEquals(1039.36, assetPrices.get("ETH"), 0.01, "Ethereum price should round to 1039.36");
+        assertEquals(0.0, assetPrices.get("ANY"), 0.01, "Any other price should equal 0.0");
+
+        verify(serviceMock).getAssets();
+    }
+
+    @Test
+    void testGetAssetPricesThrows() throws AssetServiceException, AssetStorageException {
+        when(serviceMock.getAssets()).thenReturn(assetSet);
+        when(serviceMock.getAsset("ANY")).thenThrow(AssetServiceException.class);
+
+        storage.setAssetLimit(150);
+
+        Map<String, Double> assetPrices = storage.getAssetPrices(List.of("BTC", "ETH", "ANY"));
+
+        assertEquals(19424.47, assetPrices.get("BTC"), 0.01, "Bitcoin price should round to 19424.47");
+        assertEquals(1039.36, assetPrices.get("ETH"), 0.01, "Ethereum price should round to 1039.36");
+        assertEquals(0.0, assetPrices.get("ANY"), 0.01, "Any other price should equal 0.0");
 
         verify(serviceMock).getAssets();
     }
