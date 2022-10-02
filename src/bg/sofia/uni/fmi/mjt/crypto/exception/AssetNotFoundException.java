34adac335115db876a8cd2d2e304a4c2e0cbc31d
Kaloyan Dimov <kaloyandimov99@gmail.com>
2022-10-02 20:11:05 +0300

    Add more exceptions

---
+++ src/bg/sofia/uni/fmi/mjt/crypto/exception/AssetNotFoundException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.crypto.exception;
+
+public class AssetNotFoundException extends WalletException {
+    public AssetNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/bg/sofia/uni/fmi/mjt/crypto/exception/NegativeValueException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.crypto.exception;
+
+public class NegativeValueException extends WalletException {
+    public NegativeValueException(String message) {
+        super(message);
+    }
+}
